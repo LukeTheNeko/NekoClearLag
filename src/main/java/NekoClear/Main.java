4c96af1db24d5828a2ad3580050051a366bb766d
LukeTheNeko <lucaslkzin25@gmail.com>
2023-11-28 18:55:16 -0300

    v1.0.1

--- src/main/java/NekoClear/Main.java
+++ src/main/java/NekoClear/Main.java
@@ -13,18 +13,15 @@ public final class Main extends JavaPlugin {
     public void onEnable() {
         plugin = this;
         config = new ConfigFile(this, "message");
-        Bukkit.getConsoleSender().sendMessage(c("\n" +
+        Bukkit.getConsoleSender().sendMessage(c("&f[Neko&5Clear&f] &aInicializado com sucesso." +
                 "\n" +
-                "&2Plugin Working!\n"+
-                "&2v1.0.0 by LukeTheNeko\n"+
-                "&2https://github.com/LukeTheNeko/NekoClear\n"+
-                "&5"+
+                "&f╔╗╔╔═╗╦╔═╔═╗&5╔═╗╦  ╔═╗╔═╗╦═╗\n" +
+                "&f║║║║╣ ╠╩╗║ ║&5║  ║  ║╣ ╠═╣╠╦╝\n" +
+                "&f╝╚╝╚═╝╩ ╩╚═╝&5╚═╝╩═╝╚═╝╩ ╩╩╚═\n" +
                 "\n" +
-                "  _  _ ___ _  _____     ___ _    ___   _   ___ \n" +
-                " | \\| | __| |/ / _ \\   / __| |  | __| /_\\ | _ \\\n" +
-                " | .` | _|| ' < (_) | | (__| |__| _| / _ \\|   /\n" +
-                " |_|\\_|___|_|\\_\\___/   \\___|____|___/_/ \\_\\_|_\\\n" +
-                "                                               \n"));
+                "&2v1.0.1 by LukeTheNeko\n" +
+                "&2https://github.com/LukeTheNeko/NekoClearLag\n\n"));
+
         clear.on();
         getCommand("clear").setExecutor(new clear());
     }
